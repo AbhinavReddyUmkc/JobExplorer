ad2e8ffd89fac5015730dd91725fbc5938d9a49a
Gayathri <gmmf2@umsystem.edu>
2024-12-11 22:49:47 -0600

    Team10 commit2

--- app.py
+++ app.py
@@ -21,5 +21,5 @@ def filter_data():
     filtered_data = data[data['Designation'] == selected_designation]
     return render_template('table.html', tables=filtered_data.to_html(classes='table', index=False))
 
-if __name__ == "_main_":
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
